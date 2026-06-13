cfd8532f8519b63e4ff7c7dea4c2834aa1b4f87e
zacharytyhacz <zactyh@gmail.com>
2026-06-13 19:17:29 -0400

    Added trivia questions 😏

--- discordBot/engagement/openEnded.ts
+++ discordBot/engagement/openEnded.ts
@@ -1,30 +1,17 @@
 import { TextChannel, NewsChannel, ThreadChannel } from 'discord.js';
 import { openEndedEngagementQuestions } from '../messages/openEndedEngagement';
-import { getRandomMembers } from '../utils/members';
 import crypto from 'crypto';
 
 export async function sendOpenEndedQuestion(channel: TextChannel | NewsChannel | ThreadChannel): Promise<void> {
     const randomQuestionIndex = crypto.randomInt(0, openEndedEngagementQuestions.length);
     const question = openEndedEngagementQuestions[randomQuestionIndex];
 
-    const tagPeopleThisTime = Math.random() < 0.5
-
-    if (!('guild' in channel) || !channel.guild || !tagPeopleThisTime) {
+    if (!('guild' in channel) || !channel.guild) {
         await channel.send(`
-Hey nerds
+Hey nerds - anyone know the answer?
 
 ${question}
         `.trim());
         return
     }
-    
-    const randomMembers = await getRandomMembers(channel.guild, 3);
-    const memberTags = randomMembers.map(id => `<@${id}>`).join(' ');
-    
-    await channel.send(`
-Hey ${memberTags} (randomly selected nerds) !!!
-
-${question}
-`.trim())
-
 }
